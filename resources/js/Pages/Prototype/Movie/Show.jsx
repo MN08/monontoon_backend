19a6936f09bb6d639518a83252a0aea57c89068d
Muhammad Nurhuda Hendrian <mnurhuda009@gmail.com>
2023-08-28 22:17:39 +0700

    fealt: finish slicing detail movie

--- resources/js/Pages/Prototype/Movie/Show.jsx
+++ resources/js/Pages/Prototype/Movie/Show.jsx
@@ -1,20 +1,23 @@
+import { Link } from "@inertiajs/react";
+import ReactPlayer from "react-player";
+
 export default function Show(){
     return(
-        <section className="relative w-screen mx-auto watching-page font-poppins" id="stream">
-        <video-js id="stream-video" className="w-screen h-screen overflow-hidden">
-            <source src="https://d33kv075lir7n3.cloudfront.net/Details+Screen+Part+Final.mp4" type="video/mp4" />
-            <p className="vjs-no-js text-twmdark">
-                To view this video please enable JavaScript, and consider upgrading to a
-                web browser that
-                <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
-            </p>
-        </video-js>
+        <section className="relative w-screen h-screen mx-auto watching-page font-poppins bg-form-bg" id="stream">
+        <div className="pt-[100px">
+            <ReactPlayer
+                url="https://www.youtube.com/watch?v=qJq9ZMB2Was"
+                controls
+                width={"100%"}
+                height={"650px"}
+            />
+        </div>
 
         {/* <!-- Button back to dashboard --> */}
         <div className="absolute z-20 top-5 left-5">
-            <a href="/">
+            <Link href={route("prototype.dashboard")}>
                 <img src="/icons/ic_arrow-left.svg" className="transition-all btn-back w-[46px]" alt="stream" />
-            </a>
+            </Link>
         </div>
 
         {/* <!-- Video Title --> */}
