87a849fc4e4e66206bae4ba8eec9d1c2c65033f0
Muhammad Nurhuda Hendrian <mnurhuda009@gmail.com>
2023-08-29 13:15:27 +0700

    feat : migration and seeding

---
+++ database/seeders/SubscriptionPlanTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SubscriptionPlan;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SubscriptionPlanTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $subscriptionPlan = [
+            [
+                'name' => 'Basic',
+                'price' => 249999,
+                'active_periode_in_months' => 3,
+                'features' => json_encode([
+                    'Feature 1',
+                    'Feature 2',
+                ]),
+            ],
+            [
+                'name' => 'Premium',
+                'price' => 649999,
+                'active_periode_in_months' => 4,
+                'features' => json_encode([
+                    'Feature 1',
+                    'Feature 2',
+                    'Feature 3',
+                    'Feature 4',
+                ]),
+            ],
+        ];
+
+        SubscriptionPlan::insert($subscriptionPlan);
+    }
+}
