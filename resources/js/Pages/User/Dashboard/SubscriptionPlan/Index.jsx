12e01ac221c36a56d7506bf2a46c9b14d9693b3d
Muhammad Nurhuda Hendrian <mnurhuda009@gmail.com>
2023-09-03 23:50:27 +0700

    feat: integratio Midtrans

--- resources/js/Pages/User/Dashboard/SubscriptionPlan/Index.jsx
+++ resources/js/Pages/User/Dashboard/SubscriptionPlan/Index.jsx
@@ -1,15 +1,45 @@
 import SubscriptionCard from "@/Components/SubscriptionCard";
-import AuthenticatedUser from "@/Layouts/Authenticated/Index";
-import { router } from '@inertiajs/react'
+import Authenticated from "@/Layouts/Authenticated/Index";
+import { Head, router } from '@inertiajs/react'
 
-export default function SubscriptionPlan({auth,subscriptionPlans}){
-    const selectSubscription = id =>{
+export default function SubscriptionPlan({auth,subscriptionPlans,env}){
+    const selectSubscription = (id) =>{
         router.post(route('user.dashboard.subscriptionPlan.userSubscribe',{
-            subscriptionPlan:id,
-        }));
+            subscriptionPlan: id,
+        }),
+        {},
+        {
+            only:['userSubscription'],
+            onSuccess:({ props }) => {
+                onSnapMidtrans(props.userSubscription)
+            }
+        }
+        );
+    };
+
+    const onSnapMidtrans = (userSubscription) => {
+        snap.pay(userSubscription.snap_token,{
+            // Optional
+            onSuccess: function(result){
+                router.visit(route('user.dashboard.index'));
+              },
+              // Optional
+              onPending: function(result){
+                console.log({result})
+              },
+              // Optional
+              onError: function(result){
+                console.log({result})
+              }
+        })
     }
+
     return(
-    <AuthenticatedUser user={auth.user} activePlan={auth}>
+    <Authenticated auth={auth}>
+        <Head title="Subscription Plan">
+        <script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key={env.MIDTRANS_CLIENTKEY}>
+        </script>
+        </Head>
         <div className="flex flex-col items-center py-2">
             <div className="text-black font-semibold text-[20px] mb-3">
                 Pricing for Everyone
@@ -32,6 +62,6 @@ export default function SubscriptionPlan({auth,subscriptionPlans}){
                 ))}
             </div>
         </div>
-    </AuthenticatedUser>
+    </Authenticated>
     )
 }
